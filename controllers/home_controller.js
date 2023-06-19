d70a7c8352b4284bcf3db963f7b77827d4004896
Aman <amandwdi@gmail.com>
2023-06-19 11:03:44 +0530

    this is ejs change

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Controller is working properlt in codial new.</h1>');
+    return res.render('home', {
+        title : "Home"
+    });
 }
